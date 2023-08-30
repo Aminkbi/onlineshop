bdb7a91b7ef21362e2e1e9e9845563b08643436f
Amin <aminkhanbaba51@gmail.com>
2023-08-30 19:19:31 +0330

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -30,42 +30,6 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id;
-      const url = oauth2Client.generateAuthUrl({
-        // 'online' (default) or 'offline' (gets refresh_token)
-        access_type: "offline",
-
-        // If you only need one scope you can pass it as a string
-        scope: "https://www.googleapis.com/auth/calendar",
-      });
-      const { searchParams } = new URL(url);
-      const code = searchParams.get("code");
-      //@ts-ignore
-      const { tokens } = await oauth2Client.getToken(code);
-      oauth2Client.setCredentials(tokens);
-      const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-      calendar.events.list(
-        {
-          calendarId: "primary",
-          timeMin: new Date().toISOString(),
-          maxResults: 10,
-          singleEvents: true,
-          orderBy: "startTime",
-        },
-        (err, res) => {
-          if (err) return console.log("The API returned an error: " + err);
-          const events = res?.data.items;
-          if (events?.length) {
-            console.log("Upcoming 10 events:");
-            events.map((event, i) => {
-              const start = event?.start?.dateTime || event?.start?.date;
-              console.log(`${start} - ${event.summary}`);
-            });
-          } else {
-            console.log("No upcoming events found.");
-          }
-        }
-      );
-      session.user.id = user.id;
       return session;
     },
   },
