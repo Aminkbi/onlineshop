85541cba955c5f79842849903e980e9a33441e8e
Amin <aminkhanbaba51@gmail.com>
2023-08-30 12:59:27 +0330

    test calendar

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,40 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "@/lib/env";
 import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
+import {google} from 'googleapis';
+
+
+  const googleCalendarHandler = (access_token : string|null) => {
+
+
+      const calendar = google.calendar({ version: 'v3', auth: access_token as string });
+  calendar.events.list(
+    {
+      calendarId: 'primary', // Use 'primary' for the user's primary calendar
+      timeMin: new Date().toISOString(),
+      maxResults: 10, // Number of events to retrieve
+      singleEvents: true,
+      orderBy: 'startTime',
+    },
+    (err, response) => {
+      if (err) {
+        console.error('Error fetching events:', err);
+        return;
+      }
+
+      const events = response?.data.items;
+      if (events?.length) {
+        console.log('Upcoming events:');
+        events.forEach((event) => {
+          const start = event?.start?.dateTime || event?.start?.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log('No upcoming events found.');
+      }
+    }
+  );
+  }
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma as PrismaClient),
@@ -30,7 +64,7 @@ export const authOptions: NextAuthOptions = {
       if (getToken) {
         accessToken = getToken.access_token!;
       }
-      console.log(accessToken);
+      googleCalendarHandler(accessToken);
       
       session.user.id = user.id;
       return session;
