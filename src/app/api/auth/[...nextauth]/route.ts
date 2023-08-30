9d12e02961ea6de40abf339393eab04e43ad14fd
Amin <aminkhanbaba51@gmail.com>
2023-08-30 16:30:01 +0330

    test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -30,15 +30,29 @@ export const authOptions: NextAuthOptions = {
       });
 
       let access_Token: string | null = null;
+      let refresh_Token: string | null = null;
+      let expires_at: number | null = null;
 
       if (getToken) {
         access_Token = getToken.access_token!;
+        refresh_Token = getToken.refresh_token!;
+        expires_at = getToken.expires_at!;
       }
+      const auth = new google.auth.OAuth2({
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+      });
+
+      auth.setCredentials({
+        access_token: access_Token,
+        refresh_token: refresh_Token,
+        expiry_date: expires_at,
+      });
 
       // @ts-ignore
       const calendar = google.calendar({
         version: "v3",
-        auth: access_Token,
+        auth: auth,
       });
 
       calendar.events.list(
