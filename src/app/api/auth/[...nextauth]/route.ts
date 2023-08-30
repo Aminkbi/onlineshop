5cf7a48d3f5cd3d7ce89c13f71724b3a75577d8c
Amin <aminkhanbaba51@gmail.com>
2023-08-30 16:09:20 +0330

    test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -23,7 +23,74 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async session({ session, user }) {
+      const getToken = await prisma.account.findFirst({
+        where: {
+          userId: user.id,
+        },
+      });
+
+      let access_Token: string | null = null;
+
+      if (getToken) {
+        access_Token = getToken.access_token!;
+      }
+
+      const oauth2Client = new google.auth.OAuth2(
+        env.GOOGLE_CLIENT_ID,
+
+        env.GOOGLE_CLIENT_SECRET,
+
+        `${env.NEXTAUTH_URL}/api/auth/callback/google`
+      );
+
+      // @ts-ignore
+
+      oauth2Client.setCredentials({ access_Token });
+
+      const calendar = google.calendar({
+        version: "v3",
+
+        auth: oauth2Client,
+      });
+
+      calendar.events.list(
+        {
+          calendarId: "primary", // Use 'primary' for the user's primary calendar
+
+          timeMin: new Date().toISOString(),
+
+          maxResults: 10, // Number of events to retrieve
+
+          singleEvents: true,
+
+          orderBy: "startTime",
+        },
+
+        (err, response) => {
+          if (err) {
+            console.error("Error fetching events:", err);
+
+            return;
+          }
+
+          const events = response?.data.items;
+
+          if (events?.length) {
+            console.log("Upcoming events:");
+
+            events.forEach((event) => {
+              const start = event?.start?.dateTime || event?.start?.date;
+
+              console.log(`${start} - ${event.summary}`);
+            });
+          } else {
+            console.log("No upcoming events found.");
+          }
+        }
+      );
+
       session.user.id = user.id;
+
       return session;
     },
   },
